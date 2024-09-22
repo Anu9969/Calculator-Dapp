61a0cf4297d3c60b4da7e4ef2f2f8f1e6c45a9d1
Anu9969 <sharmaanurag9969@gmail.com>
2024-09-23 00:04:08 +0530

    changes in migration and config.js file for compile and migration

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,5 @@
-var Migrations = artifacts.require("./Migrations.sol");
+var Calculator = artifacts.require("./Calculator.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(Migrations);
+  deployer.deploy(Calculator);
 };
