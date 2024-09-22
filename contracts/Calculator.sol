c9475f4b73dda916be058fb630188f14f0fb3900
Anu9969 <sharmaanurag9969@gmail.com>
2024-09-23 00:11:34 +0530

    more functions added to contract

--- contracts/Calculator.sol
+++ contracts/Calculator.sol
@@ -2,11 +2,26 @@
 pragma solidity >=0.4.22 <0.8.0;
 
 contract Calculator{
-    function addNum(uint num1 , uint num2) public returns (uint){
+
+    uint public result;
+
+    event success(string msg);
+    function addNum(uint num1 , uint num2) public pure returns (uint){
         return num1+num2;
     }
 
-        function subtractNum(uint num1 , uint num2) public returns (uint){
+    function subtractNum(uint num1 , uint num2) public pure returns (uint){
         return num1-num2;
+    }
+
+    function MulNum(uint num1 , uint num2) public {
+        return num1*num2;
+        emit success("Multiplication Successful");
+    }
+
+    function DivNum(uint num1 , uint num2) public pure returns (uint){
+        require(num2>0 , "Num cannot be zero or negative");
+        require(num1>num2 , "Num1 should be greater than Num2");
+        return num1/num2;
     }
 }
